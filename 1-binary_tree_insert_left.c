f078d7c69dbdd210f8c27d7c8d927f92f9cb8644
CarolinaLopera <carolinalopera08@gmail.com>
2021-12-01 19:16:13 -0500

    Change 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - This function create a node of the a binary tree.
+ * binary_tree_insert_left - This function create a node left in a binary tree.
  *
  * Return: Always a pointer to the node created.
  * @value: Is a integer to store in the node.
@@ -20,11 +20,12 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	new->n = value;
 	new->right = NULL;
+	new->parent = parent;
 
 	if (parent->left == NULL)
 	{
 		new->left = NULL;
-		new->parent = parent;
+		/* new->parent = parent; */
 		parent->left = new;
 	}
 	else
@@ -32,7 +33,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		current = parent->left;
 		current->parent = new;
 		new->left = current;
-		new->parent = parent;
+		/* new->parent = parent; */
 		parent->left = new;
 	}
 
