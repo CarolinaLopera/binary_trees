e820c2c3b41fe112890aecf5903177e298f08b2d
CarolinaLopera <carolinalopera08@gmail.com>
2021-12-02 00:33:52 -0500

    function print

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,25 +1,34 @@
 #include "binary_trees.h"
 
+/**
+ * print - print all nodes of a binary tree.
+ *
+ * Return: Always void.
+ * @tree: Is a pointer to root of the binaty tree.
+ */
 void print(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return;
-
-	printf("%i\n", tree->n);
-	print(tree->left);
-    print(tree->right);
+	if (tree != NULL)
+	{
+		printf("%i\n", tree->n);
+		print(tree->left);
+	    print(tree->right);
+	}
 }
 
 /**
  * binary_tree_preorder - Function to check if a node is a root.
  *
- * Return: 1 if the node is a root, otherwise 0.
- * @node: Is a node to check.
+ * Return: Always void.
+ * @tree: Is a pointer to root of the binaty tree.
+ * @func: Is a pointer to function.
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
 		return;
-	
+
+	/* binary_tree_preorder(tree, (*func)(10)); */
+
 	print(tree);
 }
