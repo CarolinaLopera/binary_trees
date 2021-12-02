65205a90994ae214abebc5dedeb24ca34f2f29ae
KevinAndresG <3348@holbertonschool.com>
2021-12-02 00:52:59 -0500

    printing each number from the tree

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,22 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * print - print all nodes of a binary tree.
- *
- * Return: Always void.
- * @tree: Is a pointer to root of the binaty tree.
- */
-void print(const binary_tree_t *tree)
-{
-	if (tree != NULL)
-	{
-		printf("%i\n", tree->n);
-		print(tree->left);
-	    print(tree->right);
-	}
-}
-
-/**
  * binary_tree_preorder - Function to check if a node is a root.
  *
  * Return: Always void.
@@ -28,7 +12,8 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree == NULL || func == NULL)
 		return;
 
-	/* binary_tree_preorder(tree, (*func)(10)); */
+	func(tree->n);
 
-	print(tree);
+	binary_tree_preorder(tree->left, *func);
+	binary_tree_preorder(tree->right, *func);
 }
